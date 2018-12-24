cabd7a7da883193217aee9795a744e229afb9c76
Paul Glezen <pglezen@isab.lacounty.gov>
2018-12-23 21:25:18 -0800

    Added SASS/CSS support.

--- readme.md
+++ readme.md
@@ -5,16 +5,16 @@ This repository provides initial configuration for a project
 incorporating
 
 * Webpack 4
-* React
-* Redux
-* Babel
+* React 16
+* Redux 4
+* Babel 7
 
 Fork this repository into a new repository to begin development
 of an React/Redux app build with Webpack 4.
 
 ## Webpack 4 Changes
 
-There were several changes I had to make to update things to Webpack 4.
+There were several changes I had to make to accommodate Webpack 4.
 
 **References**
 
@@ -32,9 +32,9 @@ complicated.
 
 ### Webpack Dev Server Host Header Checking
 
-As a security precaution, Webpack Dev Server now checkes that the HTTP 
-HOST header matches the host on which it listens.  The breaks if we are
-using localhost.  This is discussed in the following link.
+As a security precaution, Webpack Dev Server now checks that the HTTP 
+`HOST` header matches the host on which devServer listens.  This breaks 
+if we are using localhost.  This is discussed in the following links.
 
 **References**
 
@@ -42,12 +42,13 @@ using localhost.  This is discussed in the following link.
 * [Stack Overflow](https://stackoverflow.com/questions/43619644/i-am-getting-an-invalid-host-header-message-when-running-my-react-app-in-a-we/43621275#43621275)
 
 
-### CSS
+### CSS/SCSS
 
-The new kid on the block is Mini CSS Extract Plugin.
+The new kid on the block is MiniCssExtractPlugin
+(replaces [extract-text-webpack-plugin](https://www.npmjs.com/package/extract-text-webpack-plugin)).
 
 **References**
 
-* [MiniCSS Plugin](https://github.com/webpack-contrib/mini-css-extract-plugin)
+* [MiniCssExtractPlugin](https://github.com/webpack-contrib/mini-css-extract-plugin)
 * [Splitting Chunks](https://stackoverflow.com/questions/49410921/webpack-4-mini-css-extract-plugin-sass-loader-splitchunks)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
-// Figure out how to integrate mini-css-extract-plugin for webpack 4.
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-//  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
- //   entry: ['./src/styles/styles.scss', './src/app.js'],
-    entry: './src/app.js',
+    entry: [
+      './src/app.js',
+      './src/styles/styles.scss'
+    ],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
-      rules: [{
-        loader: 'babel-loader',
+      rules: [
+      {
         test: /\.js$/,
+        loader: 'babel-loader',
         exclude: /node_modules/
- //     }, {
- //       use: CSSExtract.extract({
- //         use: [
- //           {
- //             loader: 'css-loader',
- //             options: { sourceMap: true }
- //           },{
- //             loader: 'sass-loader',
- //             options: { sourceMap: true }
- //           }
- //         ]
- //       }),
- //       test: /\.s?css$/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },{
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ],
       }]
     },
-//    plugins: [
-//      CSSExtract
-//    ],
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      })
+    ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
